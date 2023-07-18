aa3af6a44e384f57dd13bf0732f2087aba663b6e
Nicolas Jacquemin <nicolas@popsell.com>
2023-07-18 15:51:44 +0200

    147 c printf parser

---
+++ Hard/147-C-Printf-Parser.ts
@@ -0,0 +1,80 @@
+/*
+  147 - C-printf Parser
+  -------
+  by Pig Fang (@g-plane) #hard #template-literal
+
+  ### Question
+
+  There is a function in C language: `printf`. This function allows us to print something with formatting. Like this:
+
+  ```c
+  printf("The result is %d.", 42);
+  ```
+
+  This challenge requires you to parse the input string and extract the format placeholders like `%d` and `%f`. For example, if the input string is `"The result is %d."`, the parsed result is a tuple `['dec']`.
+
+  Here is the mapping:
+
+  ```typescript
+  type ControlsMap = {
+    c: 'char',
+    s: 'string',
+    d: 'dec',
+    o: 'oct',
+    h: 'hex',
+    f: 'float',
+    p: 'pointer',
+  }
+  ```
+
+  > View on GitHub: https://tsch.js.org/147
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type ControlsMap = {
+    c: 'char'
+    s: 'string'
+    d: 'dec'
+    o: 'oct'
+    h: 'hex'
+    f: 'float'
+    p: 'pointer'
+  }
+
+  type TypeFromControl<C extends keyof ControlsMap> = ControlsMap[C]
+
+  type ParsePrintFormat<S extends string> = S extends `${infer first}%${infer control}${infer rest}` ?
+    control extends keyof ControlsMap ? rest extends `${infer Testfirst}%${infer Testcontrol}${infer Testrest}` ?
+    [TypeFromControl<control>, ParsePrintFormat<rest>[number]] : [TypeFromControl<control>] : 
+    rest extends `${infer Testfirst}%${infer Testcontrol}${infer Testrest}` ? [ParsePrintFormat<rest>[number]] : [] : []
+
+  type A = ParsePrintFormat<'The result is %d. %s'>
+
+  type B = ParsePrintFormat<'The result is %d.'>
+
+  type C = ParsePrintFormat<'The result is %%%d.'>
+  
+  /* _____________ Test Cases _____________ */
+  import type { Equal, Expect } from '@type-challenges/utils'
+  
+  type cases = [
+    Expect<Equal<ParsePrintFormat<''>, []>>,
+    Expect<Equal<ParsePrintFormat<'Any string.'>, []>>,
+    Expect<Equal<ParsePrintFormat<'The result is %d.'>, ['dec']>>,
+    Expect<Equal<ParsePrintFormat<'The result is %%d.'>, []>>,
+    Expect<Equal<ParsePrintFormat<'The result is %%%d.'>, ['dec']>>,
+    Expect<Equal<ParsePrintFormat<'The result is %f.'>, ['float']>>,
+    Expect<Equal<ParsePrintFormat<'The result is %h.'>, ['hex']>>,
+    Expect<Equal<ParsePrintFormat<'The result is %q.'>, []>>,
+    Expect<Equal<ParsePrintFormat<'Hello %s: score is %d.'>, ['string', 'dec']>>,
+    Expect<Equal<ParsePrintFormat<'The result is %'>, []>>,
+  ]
+  
+  /* _____________ Further Steps _____________ */
+  /*
+    > Share your solutions: https://tsch.js.org/147/answer
+    > View solutions: https://tsch.js.org/147/solutions
+    > More Challenges: https://tsch.js.org
+  */
+  
